83620440659fd52dade8e612e2da8fdeb6bb2080
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-06-12 08:24:13 +0000

    conversion & Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,3 +37,32 @@ console.log(booleanIsLoggedIN);
 "krish" => true
 */
 
+
+
+
+//------------------------------------------------------- Operations ---------------------------------------------------------------
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue); // -3
+
+/*
+Arithmatic Operations: +, -, *, **, /, %
+Comparison operators: >, <, >=, <=, !=, ==
+*/
+
+let str1 = "hello";
+let str2 = "Krish";
+console.log(str1 + str2);   // Output=> helloKrish //subtraction of strings is not possible
+
+/*
+console.log("1" + 2); // output => 12(string)
+console.log("1" + 2 + 2); // output => 122(string)
+console.log(1 + 2 + "2"); // output => 32(string)
+
+console.log(true); // true 
+console.log(+true); // 1 => Bad Practices
+*/
+
+
