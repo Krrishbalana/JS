9261514e6ad58cb163edabb58ed823e79555e455
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-06-12 09:33:23 +0000

    comparison

---
+++ 01_basics/04_Comarison.js
@@ -0,0 +1,15 @@
+// comarison operators: <, <=. >, >=, !=, ==
+// in numbers this is straight forward, preety simple.
+
+// lets discuss about some complexities.
+console.log("2" > 1);
+console.log("02" > 1);
+//suggestion => never compare 2 or more values having different dataTypes.
+
+
+
+// prefferd use => "===" this check value as well as the data type while comparison.
+// console.log("2" === 2); // this gives false because conversion will not perform conversion.
+
+
+
