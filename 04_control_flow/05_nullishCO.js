1b86dff67eb25769e8734346f332935eb31ae04f
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-06-22 18:29:17 +0000

    nullish Coalescing Operator

---
+++ 04_control_flow/05_nullishCO.js
@@ -0,0 +1,11 @@
+// Nullish Coalescing Operator (??): null undefined
+
+let val1 = 5 ?? 10;
+let val2 = null ?? 10;
+let val3 = undefined ?? 15;
+
+console.log(val1); //5
+console.log(val2); //10
+console.log(val3); //15
+
+
