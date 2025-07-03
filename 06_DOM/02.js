1c40d699cc7c5832caca5c39121d7753503c3315
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-07-03 17:49:17 +0000

    js for dom

---
+++ 06_DOM/02.js
@@ -0,0 +1,7 @@
+document.getElementById('title').setAttribute('class', 'test'); // this set the attribute class to the test and it overwrites the value of previous class value
+
+const title = document.getElementById('title');
+title.style.backgroundColor = 'red';
+title.style.borderRadius = '20px';
+title.style.padding = '15px';
+
